240f2b658f71f1d6d734fa10eebfb49dd4b9369e
Rich Boothe <richardjboothe@gmail.com>
2020-11-04 08:48:07 -0600

    change title

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Story Teller</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Story Teller
         </h1>
 
         <p className={styles.description}>
